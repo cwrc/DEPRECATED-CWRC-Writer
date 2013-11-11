0fcb269478d15c0c01a17ce52707eb28697c3477
Andrew MacDonald <andrew_james_macdonald@yahoo.com>
2013-11-11 17:46:31 -0500

    Added attributes to title tag
    Need by the CSS for proper styling.

--- src/js/tagger.js
+++ src/js/tagger.js
@@ -351,8 +351,18 @@ function Tagger(config) {
 		for (var key in attributes) {
 			if (key == 'id' || key.match(/^_/) != null) {
 				open_tag += ' '+key+'="'+attributes[key]+'"';
+			} 
+		}
+		// TODO find a better way of handling this
+		if (attributes._tag == 'title') {
+			if (attributes.level != null) {
+				open_tag += ' level="'+attributes.level+'"';
+			}
+			if (attributes.type != null) {
+				open_tag += ' type="'+attributes.type+'"';
 			}
 		}
+		
 		open_tag += '>';
 		var close_tag = '</'+tagName+'>';
 		
@@ -403,6 +413,14 @@ function Tagger(config) {
 		for (var key in attributes) {
 			if (key.match(/^_/) != null) {
 				tag.attr(key, attributes[key]);
+			}
+		}
+		if (attributes._tag == 'title') {
+			if (attributes.level != null) {
+				tag.attr('level', attributes.level);
+			}
+			if (attributes.type != null) {
+				tag.attr('type', attributes.type);
 			}
 		}
 		w.structs[id] = attributes;
