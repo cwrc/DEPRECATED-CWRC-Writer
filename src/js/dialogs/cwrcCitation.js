f24b55c90f9f4f62cdb98755e627ff133a6301b1
Andrew MacDonald <andrew_james_macdonald@yahoo.com>
2014-05-22 13:25:16 -0400

    Added missing citation files.

---
+++ src/js/dialogs/cwrcCitation.js
@@ -0,0 +1,59 @@
+define(['jquery', 'jquery-ui', 'cwrcDialogs'], function($, jqueryUi, cwrcDialogs) {
+
+// a bridge between the CWRC-Writer and the cwrcDialogs
+return function(writer) {
+	var w = writer;
+	
+	function createNewTitle(data) {
+		cD.popCreateTitle({
+			success: function(result) {
+				var r = JSON.stringify(result);
+				console.log(r);
+			},
+			error: function(errorThrown) {
+			},
+		});
+	}
+	
+	return {
+		show: function(config) {
+			if (config.entry) {
+				w.dialogManager.show('tagCitation', {
+					entry: config.entry
+				});
+			} else {
+				var query = w.editor.currentBookmark.rng.toString();
+				$('#searchEntityInput').val(query);
+				
+				cD.popSearchTitle({
+					success: function(result) {
+						if (result.id == null) {
+							result = {
+								id: 'cwrc:3b92364f-0e16-4599-bd8c-92c95a409a00',
+								name: ['Test Citation'],
+								repository: 'cwrc'
+							};
+						}
+						if ($.isArray(result.name)) {
+							result.name = result.name[0];
+						}
+						w.dialogManager.show('tagCitation', {
+							query: query,
+							cwrcInfo: result
+						});
+					},
+					error: function(errorThrown) {
+					},
+					buttons: [{
+						label : 'Create New Citation',
+						action : createNewTitle
+					}]
+				});
+			}
+		},
+		hide: function() {
+		}
+	};
+};
+
+});
