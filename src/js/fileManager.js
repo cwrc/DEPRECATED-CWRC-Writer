6c4b3361a7266bd015f4d7f080b458562de86456
Andrew MacDonald <andrew_james_macdonald@yahoo.com>
2014-07-17 15:55:58 -0400

    Strict matching for #load in fileManager

--- src/js/fileManager.js
+++ src/js/fileManager.js
@@ -136,7 +136,7 @@ return function(writer) {
 	};
 	
 	fm.loadInitialDocument = function(start) {
-		if (start.match('load')) {
+		if (start === '#load') {
 			w.dialogManager.filemanager.showLoader();
 		} else if (start.match('sample_') || start.match('template_') || start.match('blank_')) {
 			var name = start.substr(1);
