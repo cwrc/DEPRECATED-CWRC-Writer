0ca74219958a12aa07b2c3b4f1430cddc734f31f
Andrew MacDonald <andrew_james_macdonald@yahoo.com>
2014-07-24 17:56:40 -0400

    Moved Writer config to its own file

---
+++ src/js/writerConfig.js
@@ -0,0 +1,25 @@
+{
+	"cwrcRootUrl": "",
+	"schemas": {
+		"tei": {
+			"name": "CWRC Basic TEI Schema",
+			"url": "http://cwrc.ca/schemas/cwrc_tei_lite.rng",
+			"cssUrl": "css/tei_converted.css"
+		},
+		"events": {
+			"name": "Events Schema",
+			"url": "http://cwrc.ca/schemas/orlando_events.rng",
+			"cssUrl": "css/orlando_converted.css"
+		},
+		"biography": {
+			"name": "Biography Schema",
+			"url": "http://cwrc.ca/schemas/orlando_biography.rng",
+			"cssUrl": "css/orlando_converted.css"
+		},
+		"writing": {
+			"name": "Writing Schema",
+			"url": "http://cwrc.ca/schemas/orlando_writing.rng",
+			"cssUrl": "css/orlando_converted.css"
+		}
+	}
+}
