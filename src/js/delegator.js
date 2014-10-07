f55542ec34bde516afe1cbb3d19c2781e0453385
Andrew MacDonald <andrew_james_macdonald@yahoo.com>
2014-10-07 14:38:18 -0400

    Forced use of dev templates for now

--- src/js/delegator.js
+++ src/js/delegator.js
@@ -269,8 +269,9 @@ return function(writer) {
         var octo = new Octokit({token: '15286e8222a7bc13504996e8b451d82be1cba397'});
         var templateRepo = octo.getRepo('cwrc', 'CWRC-Writer-Templates');
         // if we're on development then also get the templates development branch
+        var forceDev = true;
         var isDev = window.location.pathname.indexOf('/dev/') !== -1;
-        if (isDev) {
+        if (forceDev || isDev) {
             return templateRepo.getBranch('development');
         } else {
             return templateRepo.getBranch('master');
