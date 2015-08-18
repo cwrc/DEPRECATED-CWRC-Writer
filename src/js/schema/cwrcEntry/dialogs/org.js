3488a23ecf06b22370f0090a2991418d5ed6a012
Andrew MacDonald <andrew_james_macdonald@yahoo.com>
2015-08-18 16:00:25 -0400

    orgtype added to orgname
    #357

--- src/js/schema/cwrcEntry/dialogs/org.js
+++ src/js/schema/cwrcEntry/dialogs/org.js
@@ -13,6 +13,35 @@ return function(id, writer) {
             '<label for="'+id+'_input">Standard name</label>'+
             '<input type="text" id="'+id+'_input" data-type="textbox" data-mapping="STANDARD" />'+
         '</div>'+
+        '<div>'+
+            '<p>Organization type:</p>'+
+            '<select data-type="select" data-mapping="ORGTYPE">'+
+                '<option value=""></option>'+
+                '<option value="labour">labour</option>'+
+                '<option value="club or society">club or society</option>'+
+                '<option value="political">political</option>'+
+                '<option value="academic">academic</option>'+
+                '<option value="research">research</option>'+
+                '<option value="corporate">corporate</option>'+
+                '<option value="activist">activist</option>'+
+                '<option value="governmental">governmental</option>'+
+                '<option value="military">military</option>'+
+                '<option value="law-enforcement">law-enforcement</option>'+
+                '<option value="professional">professional</option>'+
+                '<option value="sporting">sporting</option>'+
+                '<option value="medical">medical</option>'+
+                '<option value="altruistic">altruistic</option>'+
+                '<option value="non-profit">non-profit</option>'+
+                '<option value="artistic">artistic</option>'+
+                '<option value="dramatic">dramatic</option>'+
+                '<option value="ethnic">ethnic</option>'+
+                '<option value="literary">literary</option>'+
+                '<option value="musical">musical</option>'+
+                '<option value="religious">religious</option>'+
+                '<option value="publishing">publishing</option>'+
+                '<option value="school">school</option>'+
+            '</select>'+
+        '</div>'+
         '<input type="hidden" id="'+id+'_ref" data-type="hidden" data-mapping="REF"/>'+
     '</div>';
     
@@ -20,7 +49,7 @@ return function(id, writer) {
         writer: w,
         id: id,
         width: 200,
-        height: 150,
+        height: 200,
         type: 'org',
         title: 'Tag Organization',
         html: html
