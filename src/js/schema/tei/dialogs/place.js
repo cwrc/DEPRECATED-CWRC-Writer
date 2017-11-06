635e9309be2129fe2bedbcdfad2c47d23553b915
Andrew <ajmacdonald@users.noreply.github.com>
2017-11-06 12:33:22 -0500

    change values of cert attribute
    #485

--- src/js/schema/tei/dialogs/place.js
+++ src/js/schema/tei/dialogs/place.js
@@ -15,10 +15,10 @@ return function(id, writer) {
         '</div>'+
         '<div id="'+id+'_certainty" data-transform="buttonset" data-type="radio" data-mapping="cert">'+
             '<p>This identification is:</p>'+
-            '<input type="radio" id="'+id+'_definite" name="'+id+'_id_certainty" value="definite" data-default="true" /><label for="'+id+'_definite">Definite</label>'+
-            '<input type="radio" id="'+id+'_reasonable" name="'+id+'_id_certainty" value="reasonably certain" /><label for="'+id+'_reasonable">Reasonably Certain</label>'+
-            '<input type="radio" id="'+id+'_probable" name="'+id+'_id_certainty" value="probable" /><label for="'+id+'_probable">Probable</label>'+
-            '<input type="radio" id="'+id+'_speculative" name="'+id+'_id_certainty" value="speculative" /><label for="'+id+'_speculative">Speculative</label>'+
+            '<input type="radio" id="'+id+'_high" name="'+id+'_id_certainty" value="high" data-default="true" /><label for="'+id+'_high">High</label>'+
+            '<input type="radio" id="'+id+'_low" name="'+id+'_id_certainty" value="low" /><label for="'+id+'_low">Low</label>'+
+            '<input type="radio" id="'+id+'_medium" name="'+id+'_id_certainty" value="medium" /><label for="'+id+'_medium">Medium</label>'+
+            '<input type="radio" id="'+id+'_unknown" name="'+id+'_id_certainty" value="unknown" /><label for="'+id+'_unknown">Unknown</label>'+
         '</div>'+
         '<div>'+
             '<p>Precision of location of place name:</p>'+
